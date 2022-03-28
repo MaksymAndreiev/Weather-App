9c73e7f67dbea9d1f010ae59fc140dd7b5e8c65c
Maksym Andreiev <maxoggyha@gmail.com>
2022-03-28 21:08:33 +0200

    Update alerts, tooltips and input.

--- app.py
+++ app.py
@@ -6,17 +6,11 @@ import requests
 import json
 import datetime
 
-# import jinja2
-#
-# loader = jinja2.FileSystemLoader('/tmp')
-# env = jinja2.Environment(autoescape=True, loader=loader)
-# env.filters['all'] = all
-
 app = Flask(__name__)
 # db = create_engine('sqlite:///weather.db?check_same_thread=False')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 db = SQLAlchemy(app)
-api_key = '80d2dcec03133dc269c9a17fea6df313'
+api_key = ['80d2dcec03133dc269c9a17fea6df313', 'be239b2e9342bb6379f7f5a1a1f81a71']
 app.secret_key = 'secret_key'
 
 
@@ -43,19 +37,49 @@ def index():
         hour = datetime.datetime.now(tz=tz).time().hour
         return hour, time, day
 
+    def get_date_name(dt):
+        date = datetime.datetime.fromtimestamp(dt)
+        day = date.date().strftime("%a")
+        return day
+
     weather = []
     cities = City.query.all()
     for city in cities:
         city_name = city.name
-        # units in api
-        data = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric')
+
+        try:
+            data = requests.get(
+                f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key[0]}&units=metric')
+            data.raise_for_status()
+        except ConnectionError:
+            data = requests.get(
+                f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key[1]}&units=metric')
+
         lat = json.loads(data.content)["coord"].get("lat")
         lon = json.loads(data.content)["coord"].get("lon")
-        data_forecast = requests.get(
-            f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=current,minutely,hourly,'
-            f'alerts&appid={api_key}&units=metric')
-        days_weather = list(json.loads(data_forecast.content)["daily"][i].get("temp").get("day") for i in range(7))
-        days_name = []
+
+        try:
+            data_forecast = requests.get(
+                f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=current,minutely,hourly,'
+                f'alerts&appid={api_key[0]}&units=metric')
+        except ConnectionError:
+            data_forecast = requests.get(
+                f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=current,minutely,hourly,'
+                f'alerts&appid={api_key[1]}&units=metric')
+
+        days_weather = list(
+            int(json.loads(data_forecast.content)["daily"][i].get("temp").get("day")) for i in range(1, 8))
+        days_names = ['i' for i in range(7)]
+        for i in range(1, 8):
+            days_names[i - 1] = get_date_name(json.loads(data_forecast.content)["daily"][i].get('dt'))
+
+        tooltip_info = [
+            list(int(json.loads(data_forecast.content)["daily"][i].get("temp").get("day")) for i in range(1, 8)),
+            list(int(json.loads(data_forecast.content)["daily"][i].get("temp").get("night")) for i in range(1, 8)),
+            list(int(json.loads(data_forecast.content)["daily"][i].get("feels_like").get("day")) for i in range(1, 8)),
+            list(int(json.loads(data_forecast.content)["daily"][i].get("feels_like").get("night")) for i in range(1, 8))
+        ]
+
         dict_with_weather_info = {
             "name": json.loads(data.content)["name"],
             "state": {k: v for e in json.loads(data.content)["weather"] for (k, v) in e.items()}.get("main"),
@@ -64,10 +88,13 @@ def index():
             "wind": int(json.loads(data.content)["wind"].get("speed")),
             "pressure": json.loads(data.content)["main"].get("pressure"),
             "humidity": json.loads(data.content)["main"].get("humidity"),
+            "precipitation": int(json.loads(data_forecast.content)["daily"][0].get("pop") * 100),
             "time": get_date(json.loads(data.content)['timezone'])[0],
             "curTime": get_date(json.loads(data.content)['timezone'])[1],
             "date": get_date(json.loads(data.content)['timezone'])[2],
             "days": days_weather,
+            "names": days_names,
+            "tooltip": tooltip_info,
             "id": f"{city.id}",
         }
         weather.append(dict_with_weather_info)

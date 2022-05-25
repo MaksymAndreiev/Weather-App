45a47022438a4a579156de4657b1fa97d6ec0b55
Maksym Andreiev <maxoggyha@gmail.com>
2022-05-25 02:00:16 +0200

    Add country controller

---
+++ controllers/country.py
@@ -0,0 +1,23 @@
+from models.Country import Country
+
+
+def get_all_countries():
+    """
+    Get list of all records
+    """
+    all_countries = Country.query.all()
+    return all_countries
+
+
+def add_country(country_name):
+    countries = get_all_countries()
+    for country in countries:
+        if country.name == country_name:
+            pass
+    else:
+        Country.create(name=country_name)
+
+
+# def delete(country_id):
+#     City.delete(country_id)
+#     return redirect('/')

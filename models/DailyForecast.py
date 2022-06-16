70aac57ce1e00bac8a56464d9e9b2f0d985b8765
Maksym Andreiev <maxoggyha@gmail.com>
2022-06-16 23:54:14 +0900

    Add DELETE CASCADE

--- models/DailyForecast.py
+++ models/DailyForecast.py
@@ -5,10 +5,10 @@ from models.base import Model
 class DailyForecast(Model, db.Model):
     __tablename__ = 'daily_forecast'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    city_id = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
+    city_id = db.Column(db.Integer, db.ForeignKey('city.id', ondelete='CASCADE'), nullable=False)
     day_id = db.Column(db.Integer, nullable=False)
     day_name = db.Column(db.String(3), nullable=False)
-    weather_status_id = db.Column(db.Integer, db.ForeignKey('weather_status.id'), nullable=False)
+    weather_status_id = db.Column(db.Integer, db.ForeignKey('weather_status.id', ondelete='CASCADE'), nullable=False)
     day_temperature = db.Column(db.Integer, nullable=False)
     night_temperature = db.Column(db.Integer, nullable=False)
     feels_like_day = db.Column(db.Integer, nullable=False)
--- models/HourlyForecast.py
+++ models/HourlyForecast.py
@@ -5,8 +5,8 @@ from models.base import Model
 class HourlyForecast(Model, db.Model):
     __tablename__ = 'hourly_forecast'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    city_id = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
-    weather_status_id = db.Column(db.Integer, db.ForeignKey('weather_status.id'), nullable=False)
+    city_id = db.Column(db.Integer, db.ForeignKey('city.id', ondelete='CASCADE'), nullable=False)
+    weather_status_id = db.Column(db.Integer, db.ForeignKey('weather_status.id', ondelete='CASCADE'), nullable=False)
     temperature = db.Column(db.Integer, nullable=False)
     wind_speed = db.Column(db.Float(decimal_return_scale=2), nullable=False)
     pressure = db.Column(db.Float(decimal_return_scale=2), nullable=False)

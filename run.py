5a0cff74fdd9f98c53844f1f71243c9b9e2511c5
MaffaNja <92969417+MaffaNja@users.noreply.github.com>
2022-06-17 21:04:31 +0900

    Fix doc

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
-import doctest
-
 import core
 
 app = core.create_app()
-
-
-def load_tests(loader, tests, ignore):
-    tests.addTests(doctest.DocTestSuite(core.routes))
-    return tests
 
 
 if __name__ == "__main__":

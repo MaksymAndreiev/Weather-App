4acd6a0b138a133135aea9c1a917687d99060f97
Maksym Andreiev <maxoggyha@gmail.com>
2022-06-18 02:28:36 +0900

    Small changes

--- models/Country.py
+++ models/Country.py
@@ -6,6 +6,4 @@ class Country(Model, db.Model):
     __tablename__ = 'country'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), unique=True, nullable=False)
-    # добавить полное имя страны
     cities = db.relationship('City', backref='country', lazy='dynamic', cascade='all, delete')
-    # users = db.relationship('Users', bakcref='country', lazy='dynamic', cascade='all, delete')
